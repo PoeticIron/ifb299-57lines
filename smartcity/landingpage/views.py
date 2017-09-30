9f36a4587d69f37a1c31691b8383aa9c928338f8
PoeticIron <nmestone@gmail.com>
2017-09-30 11:22:43 +1000

    Revert to previous, working status
    Previous commit deleted all of the operational work added to
    registration and login pages. Reverting these changes should fix this.

--- smartcity/landingpage/views.py
+++ smartcity/landingpage/views.py
@@ -1,7 +1,16 @@
 from django.shortcuts import render
 from django.views.generic import TemplateView
+from django.contrib.auth import authenticate
+from django.http import HttpResponse, HttpResponseRedirect
 
 # Create your views here.
 class LoginPageView(TemplateView):
     def get(self, request, **kwargs):
         return render(request, 'loginpage.html', context=None)
+def login(request):
+	if request.method == 'POST':
+		user = authenticate(username=request.POST.get("uname"), password=request.POST.get("psw"))
+		if user is not None:
+			return HttpResponseRedirect('/mainpage')
+		else:
+			return HttpResponseRedirect('/')
--- smartcity/registerpage/views.py
+++ smartcity/registerpage/views.py
@@ -1,8 +1,27 @@
 from django.shortcuts import render
 from django.views.generic import TemplateView
+from .forms import RegForm
+from django.contrib.auth.models import User
+from django.http import HttpResponse, HttpResponseRedirect
 
 # Create your views here.
 class RegisterPageView(TemplateView):
     def get(self, request, **kwargs):
-        return render(request, 'registerpage.html', context=None)
+        if request.method == 'POST':
+            form = RegForm(request.POST)
+            if form.is_valid():
+                user = User.objects.create_user(request.POST.get("username"),request.POST.get("email"),request.POST.get("password"))
+                return HttpResponseRedirect('/mainpage')
+        else:
+            form=RegForm()
+        return render(request, 'registerpage.html', {'form':form})
 
+def register(request):
+	if request.method == 'POST':
+		form = RegForm(request.POST)
+		if form.is_valid():
+			user = User.objects.create_user(request.POST.get("username"),request.POST.get("email"),request.POST.get("password"))
+			return HttpResponseRedirect('/mainpage')
+	else:
+		form=RegForm()
+	return render(request, 'name.html', {'form':form})
