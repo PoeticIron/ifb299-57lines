0efa862edb81cffe07077a6e40f39194868a88a0
nishisandali <nishi.sandali@gmail.com>
2017-10-21 23:46:38 +1000

    Delete urls.py

--- smartcity/updatepage/urls.py
+++
@@ -1,7 +0,0 @@
-from django.conf.urls import url
-from updatepage import views
-
-urlpatterns = [
-    url(r'^$', views.UpdatePageView.as_view()),
-        url(r'^update', views.update, name='update'),
-]
