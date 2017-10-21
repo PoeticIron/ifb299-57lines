024d761cabbe6d9f6a354dd10fbc3aa99f8ea2c2
nishisandali <nishi.sandali@gmail.com>
2017-10-21 23:47:57 +1000

    Delete urls.py

--- urls.py
+++
@@ -1,8 +0,0 @@
-from django.conf.urls import url
-from updatepage import views
-
-urlpatterns = [
-    url(r'^$', views.UpdatePageView.as_view()),
-        url(r'^update', views.update, name='update'),
-        url(r'^delete', views.delete, name='delete'),
-]
