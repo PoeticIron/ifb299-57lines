8fab4241cf186251e0406b1b602bb9d68ec7628e
PoeticIron <nmestone@gmail.com>
2017-10-27 18:21:38 +1000

    Register Page User Type addition

--- smartcity/registerpage/forms.py
+++ smartcity/registerpage/forms.py
@@ -1,13 +1,30 @@
 from django import forms
+from django.contrib.auth.models import User
+from registerpage.models import Profile
 
+UserTypes = (
+    ('1', 'Student'),
+    ('2', 'Tourist'),
+    ('3', 'Businessperson'),
+)
 
 class RegForm(forms.Form):
+		model = User
 		username = forms.CharField(label='Username', max_length=150)
 		firstname = forms.CharField(max_length=100)
 		lastname = forms.CharField(max_length=100)
 		email = forms.EmailField()
 		password = forms.CharField(widget=forms.PasswordInput)
+		Confirm_password = forms.CharField(widget=forms.PasswordInput)
 		widgets = {
 			'password': forms.PasswordInput(),
 		}
-			
+
+class TypeForm(forms.ModelForm):
+	class Meta:
+		model = Profile
+		fields = ('userType',)
+class UserForm(forms.ModelForm):
+    class Meta:
+        model = User
+        fields = ('username','first_name', 'last_name', 'email')
--- smartcity/registerpage/views.py
+++ smartcity/registerpage/views.py
@@ -1,27 +1,33 @@
 from django.shortcuts import render
 from django.views.generic import TemplateView
-from .forms import RegForm
+from .forms import RegForm, TypeForm
 from django.contrib.auth.models import User
 from django.http import HttpResponse, HttpResponseRedirect
+from .models import Profile
 
 # Create your views here.
 class RegisterPageView(TemplateView):
     def get(self, request, **kwargs):
         if request.method == 'POST':
             form = RegForm(request.POST)
+            typeForm = TypeForm(request.POST)
             if form.is_valid():
                 user = User.objects.create_user(request.POST.get("username"),request.POST.get("email"),request.POST.get("password"))
                 return HttpResponseRedirect('/mainpage')
         else:
             form=RegForm()
-        return render(request, 'registerpage.html', {'form':form})
+            typeForm = TypeForm(request.POST)
+        return render(request, 'registerpage.html', {'form':form, 'typeForm': typeForm})
 
 def register(request):
 	if request.method == 'POST':
 		form = RegForm(request.POST)
+		typeForm = TypeForm(request.POST)
 		if form.is_valid():
-			user = User.objects.create_user(request.POST.get("username"),request.POST.get("email"),request.POST.get("password"))
+			fields = request.POST
+			user = User.objects.create_user(fields.get("username"),request.POST.get("email"),request.POST.get("password"))
 			return HttpResponseRedirect('/mainpage')
 	else:
 		form=RegForm()
-	return render(request, 'registerpage.html', {'form':form})
+		typeForm = TypeForm(request.POST)
+	return render(request, 'registerpage.html', {'form':form, 'typeForm': typeForm})
