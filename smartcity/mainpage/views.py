a965d9688932cc3b5f2023c09438ba04ec0450b4
PoeticIron <nmestone@gmail.com>
2017-10-31 16:34:35 +1000

    map markers for colleges on mainpage

--- smartcity/mainpage/views.py
+++ smartcity/mainpage/views.py
@@ -2,13 +2,22 @@ from django.shortcuts import render
 from django.views.generic import TemplateView
 from django.http import HttpResponse, HttpResponseRedirect
 
-from dbview.models import Colleges
+from dbview.models import *
 
 # Create your views here.
 class MainPageView(TemplateView):   
     def get(self, request, **kwargs):
         colleges = Colleges.objects.all()
-        return render(request, 'mainpage.html', {'colleges': colleges})
+        industries = Industries.objects.all()
+        hotels = Hotels.objects.all()
+        parks = Parks.objects.all()
+        zoos = Zoos.objects.all()
+        museums = Museums.objects.all()
+        malls = Malls.objects.all()
+        restaurants = Restaurants.objects.all()
+        libraries = Libraries.objects.all()
+
+        return render(request, 'mainpage.html', {'colleges': colleges, 'libs': libraries, 'industries':industries, 'hotels':hotels, 'parks':parks, 'zoos':zoos, 'museums':museums,'malls':malls,'restaurants':restaurants,'libraries':libraries	})
 		
 
         
